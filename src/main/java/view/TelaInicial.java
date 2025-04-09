7d05185704d1fd6b7837a1840a0f9b89fc27b03f
v1trovao <viitrovao@gmail.com>
2025-04-09 14:18:11 -0300

    Revisão Geral do Código, alguns comentários

--- src/main/java/view/TelaInicial.java
+++ src/main/java/view/TelaInicial.java
@@ -1,15 +1,18 @@
 package view;
 
+// Camada view interagindo diretamente com o model?
 import model.Registro;
 import model.Tarefa;
 import model.TipoRegistro;
 
+// Utilitarios de leitura e manipulacao de arrays na view msm?
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class TelaInicial {
-    List<Tarefa> tarefas = new ArrayList<>();
+
+    // Separar a persistência de dados em local separado
     List<Registro> registros = new ArrayList<>();
 
     Scanner leitor = new Scanner(System.in);
@@ -17,6 +20,8 @@ public class TelaInicial {
     public void exibirMenu() {
         System.out.println("------ AGENDA -------");
 
+        // Deixar mais claro para o usuário oq ele pode fazer na tela
+        // Como finalizar o programa?
         System.out.println("""
                 1 - Novo Registro
                 2 - Excluir Registro
@@ -24,6 +29,7 @@ public class TelaInicial {
                 4 - Listar Agenda""");
         String opcao = leitor.nextLine();
 
+        // Se possivel, tentar separar cada caso em uma view específica (ex.: JanelaAdicionarRegistro)
         switch (opcao) {
             case "1":
                 adicionarRegistro();
@@ -41,27 +47,11 @@ public class TelaInicial {
         }
     }
 
-    public void adicionarTarefa() {
-        System.out.print("Titulo: ");
-        String titulo = leitor.nextLine();
-        System.out.print("Descricao: ");
-        String descricao = leitor.nextLine();
-        System.out.print("Data: ");
-        String data = leitor.nextLine();
-        System.out.print("Hora: ");
-        double hora = leitor.nextDouble();
-        leitor.nextLine(); // Consome a quebra de linha deixada pelo nextDouble()
-
-        int id = tarefas.size() + 1;
-        Tarefa tarefa = new Tarefa(titulo, descricao, data, id, hora);
-        tarefas.add(tarefa);
-
-        System.out.println("Tarefa adicionada com sucesso!");
-    }
-
     public void adicionarRegistro() {
 
         // Tratamento de excecção para entradas diferentes
+        // É melhor o usuário escrever o tipo ou selecionar por numero?
+        // 1 - Tarefa, 2 - Evento e 3 - Reunião
         System.out.println("Digite o tipo (TAREFA, EVENTO ou REUNIÃO");
         TipoRegistro tipo = TipoRegistro.valueOf(leitor.nextLine().toUpperCase());
         System.out.print("Titulo: ");
@@ -71,20 +61,10 @@ public class TelaInicial {
         System.out.print("Data: ");
         String data = leitor.nextLine();
 
+        // Essa lógica poderia estar na camada de serviço?
         int id = registros.size() + 1;
         Registro novoRegistro = new Registro(id, titulo, data, descricao, tipo);
         registros.add(novoRegistro);
-    }
-
-    public void listarTarefas() {
-        System.out.println("Cheguei aqui");
-        if (tarefas.isEmpty()) {
-            System.out.println("Nenhuma tarefa cadastrada.");
-        } else {
-            for (Tarefa tarefa : tarefas) {
-                System.out.println(tarefa);
-            }
-        }
     }
 
     public void listarRegistros() {
@@ -97,10 +77,9 @@ public class TelaInicial {
         }
     }
 
-    public void removerTarefa() {
-    }
-
     public void removerRegistro() {
+
+        // Essa verificação é melhor estruturada na view ou em service?
         if (registros.isEmpty()) {
             System.out.println("Nenhum registro cadastrado.");
         }
@@ -108,13 +87,16 @@ public class TelaInicial {
             System.out.println("Remoção de Registro");
             listarRegistros();
             System.out.println("Digite o ID do registro que deseja remover");
-            System.out.println(registros.size());
+            System.out.println(registros.size()); // Vai imprimir o tamanho da lista?
+
+            // Leitura com nextInt() e nextLine() para lidar com o /n
+            // Talvez fosse interessante usar a leitura de string, e depois converter pra int?
             int opcao = leitor.nextInt();
             leitor.nextLine();
 
             if (opcao > 0) {
                 // Considerar os casos
-                // - Remover um registro, e atualizar indíces dos demais registros
+                // Remover um registro, e atualizar indíces dos demais registros
 
                 // Remover primeiro elemento da lista
                 if (opcao == 1) {
@@ -148,9 +130,6 @@ public class TelaInicial {
         }
     }
 
-    public void editarTarefa() {
-    }
-
     public void editarRegistro() {
         if (registros.isEmpty()) {
             System.out.println("Nenhum registro cadastrado.");
@@ -160,6 +139,8 @@ public class TelaInicial {
             int opcao = leitor.nextInt();
             leitor.nextLine();
             if (opcao > 0 && opcao <= registros.size()) {
+                // Alterar múltiplos campos, mas deixando o usuario
+                // escolher qual alterar e qual manter
                 Registro registro = registros.get(opcao-1);
                 System.out.print(registro.getTitulo());
                 String titulo = leitor.nextLine();
